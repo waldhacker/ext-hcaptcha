0d240dfc5b4cca0ce34d584bdc6c3122b304c1ad
Ralf Zimmermann <hello@waldhacker.dev>
2021-04-22 21:32:20 +0200

    [TASK] define version 1.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,17 +4,17 @@ $EM_CONF[$_EXTKEY] = [
     'title'            => 'hCaptcha for EXT:form',
     'description'      => 'Privacy friendly alternative to Google\'s hcaptcha for EXT:form',
     'category'         => 'frontend',
-    'author'           => 'Susanne Moog',
-    'author_email'     => 'look@susi.dev',
+    'author'           => 'Susanne Moog, Ralf Zimmermann',
+    'author_email'     => 'look+typo3@susi.dev, hello@waldhacker.dev',
     'author_company'   => '',
     'state'            => 'stable',
     'uploadfolder'     => '0',
     'clearCacheOnLoad' => 1,
-    'version'          => '1.0.0',
+    'version'          => '1.1.0',
     'constraints'      => [
         'depends' => [
-            'typo3' => '10.4.0-10.4.99',
-            'form' => '10.4.0-10.4.99'
+            'typo3' => '10.4.0-11.5.99',
+            'form' => '10.4.0-11.5.99'
         ]
     ]
 ];
