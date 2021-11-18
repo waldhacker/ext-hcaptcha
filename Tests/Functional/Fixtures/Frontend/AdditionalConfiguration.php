d11deeec5c7c64e69aa43edb12e3c60e7815c001
Ralf Zimmermann <hello@waldhacker.dev>
2021-11-19 00:39:39 +0100

    [TASK] Remove unused code

--- Tests/Functional/Fixtures/Frontend/AdditionalConfiguration.php
+++ Tests/Functional/Fixtures/Frontend/AdditionalConfiguration.php
@@ -8,23 +8,6 @@ $GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive(
     $GLOBALS['TYPO3_CONF_VARS'],
     [
         'LOG' => [
-            'writerConfiguration' => [
-                \TYPO3\CMS\Core\Log\LogLevel::ERROR => [
-                    \TYPO3\CMS\Core\Log\Writer\FileWriter::class => [],
-                ],
-                \TYPO3\CMS\Core\Log\LogLevel::WARNING => [
-                    \TYPO3\CMS\Core\Log\Writer\FileWriter::class => [],
-                ],
-                \TYPO3\CMS\Core\Log\LogLevel::NOTICE => [
-                    \TYPO3\CMS\Core\Log\Writer\FileWriter::class => [],
-                ],
-                \TYPO3\CMS\Core\Log\LogLevel::INFO => [
-                    \TYPO3\CMS\Core\Log\Writer\FileWriter::class => [],
-                ],
-                \TYPO3\CMS\Core\Log\LogLevel::DEBUG => [
-                    \TYPO3\CMS\Core\Log\Writer\FileWriter::class => [],
-                ],
-            ],
         ],
         'MAIL' => [
             'defaultMailFromAddress' => 'hello@waldhacker.dev',
@@ -32,14 +15,6 @@ $GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive(
             'transport' => 'mbox',
             'transport_spool_type' => 'file',
             'transport_spool_filepath' => \Waldhacker\Hcaptcha\Tests\Functional\FunctionalTestCase::MAIL_SPOOL_FOLDER,
-        ],
-        'SC_OPTIONS' => [
-            'Core/TypoScript/TemplateService' => [
-                'runThroughTemplatesPostProcessing' => [
-                    // Register hooks for frontend test
-                    //'FunctionalTest' => \TYPO3\TestingFramework\Core\Functional\Framework\Frontend\Hook\TypoScriptInstructionModifier::class . '->apply',
-                ],
-            ],
         ],
     ]
 );
