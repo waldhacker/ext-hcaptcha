84b10f8625cab99987ed401d64e475f81975490a
Ralf Zimmermann <hello@waldhacker.dev>
2021-11-19 00:40:13 +0100

    [TASK] Remove unused code

--- Tests/Functional/Fixtures/Frontend/AdditionalConfiguration.php
+++ Tests/Functional/Fixtures/Frontend/AdditionalConfiguration.php
@@ -7,8 +7,6 @@ defined('TYPO3') or die();
 $GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive(
     $GLOBALS['TYPO3_CONF_VARS'],
     [
-        'LOG' => [
-        ],
         'MAIL' => [
             'defaultMailFromAddress' => 'hello@waldhacker.dev',
             'defaultMailFromName' => 'waldhacker',
